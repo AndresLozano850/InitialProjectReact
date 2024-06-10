84e5033945a647d54bf75dcefa4289ed04f25cdd
Andrés Lozano <camilolozano850@gmail.com>
2024-06-10 15:14:44 -0500

    UseState -> hook en react

--- src/Component.jsx
+++ src/Component.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React,{ useState } from 'react'
 
 export default function Component() {
+    const [text,setText]= useState();
+    const [update,setUpdate]= useState();
+
+    const textOnchange = (event) => {
+        setText(event.target.value); // Cuando Onchange Cambie se llama Set
+    }
+    const buttonOnclick = () => {
+        setUpdate(text); // Cuando Onchange Cambie se llama Set
+    }
+
   return (
-    <div>My Component Andres Lozano</div>
+    <div>
+        <input type="text" value={text} onChange={textOnchange}/>
+        <button onClick={buttonOnclick}>Actualzizar</button>
+        <p>Texto input: {text}</p>
+        <p>Texto Actualizado {update} </p>
+       <input type="text" />
+    </div>
   )
 }
